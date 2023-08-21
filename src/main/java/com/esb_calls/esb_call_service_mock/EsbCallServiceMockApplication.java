3d05aee3f2758b9f5cd403136daa81e9eb580f9e
andjela.zizic <andjela.zizic@nlb.rs>
2023-08-21 10:26:39 +0200

    first commit

---
+++ src/main/java/com/esb_calls/esb_call_service_mock/EsbCallServiceMockApplication.java
@@ -0,0 +1,13 @@
+package com.esb_calls.esb_call_service_mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsbCallServiceMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsbCallServiceMockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esb_calls/esb_call_service_mock/EsbCallServiceMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esb_calls.esb_call_service_mock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsbCallServiceMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
